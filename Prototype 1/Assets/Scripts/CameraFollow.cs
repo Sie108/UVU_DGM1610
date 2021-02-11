3b7819fee7d82a2e40ef9a7599dcdcffc9677eba
Sierra Weber <10889154>
2021-02-11 00:41:13 -0700

    Prototype 1

---
+++ Prototype 1/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0,7,-11);
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
