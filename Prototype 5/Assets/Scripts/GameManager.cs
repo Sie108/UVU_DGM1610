f47b1dc234ddb1710e99a1ce86d842ec7be816e5
Sierra Weber <10889154>
2021-04-21 17:21:39 -0600

    Prototype 5

---
+++ Prototype 5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public bool isGameActive;
+    public List<GameObject> targets;
+    private float spawnRate = 1.0f;
+    
+    private int score;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public Button resetButton;
+
+    public GameObject titleScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while(isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0,targets.Count);
+            Instantiate(targets[index]);
+
+        }
+    }
+    //This is the code that makes the Score add when you destroy the objects
+   public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+
+        scoreText.text = "Score: " + score;
+        
+    }
+    //This is the code that makes the game over text appear when the objects go untouched
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+        resetButton.gameObject.SetActive(true);
+    }
+    //This is the code made to make the reset button work
+    public void ResetGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void StartGame(int difficulty)
+    {
+        UpdateScore(0);
+        scoreText.text = "Score: " + score;
+        isGameActive = true;
+        titleScreen.gameObject.SetActive(false);
+        spawnRate /= difficulty;
+        StartCoroutine(SpawnTarget());
+    }
+}
